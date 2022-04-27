619054e60acaafb74a94ce77fc40bc80aa4b060b
Antje Exner <antjeexner@Antjes-Air.fritz.box>
2022-04-27 09:16:06 +0200

    install storybook

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
