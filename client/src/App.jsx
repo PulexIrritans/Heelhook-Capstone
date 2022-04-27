fc7211a792037b6018efaba087ee6f67376be618
Antje Exner <antjeexner@Antjes-Air.fritz.box>
2022-04-27 13:01:33 +0200

    add basic BoulderCard component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react';
 import './App.css';
+import BoulderCard from './components/BoulderCard';
 
 function App() {
  
   const bouldersList = [
     {
-      id: 1,
+      number: 1,
       name: 'Black Widow and the people',
       sector: 'Monkey Island',
       level: '5',
       handle_color: 'black',
       setter: 'Martin Pagels',
       tags: ['Dyno', 'Sloper'],
-      img_start: 'image.jpg',
+      img_start: 'Boulder-girl.jpg',
       img_all: 'imageAll.jpg',
       weighting: 100,
       number_of_likes: 5,
@@ -21,14 +21,14 @@ function App() {
       climbers: [],
     },
     {
-      id: 2,
+      number: 2,
       name: 'Orang Utan',
       sector: 'Monkey Island',
       level: '3',
       handle_color: 'blue',
       setter: 'Stefan Korder',
       tags: ['Sloper'],
-      img_start: 'image.jpg',
+      img_start: 'Boulder-girl.jpg',
       img_all: 'imageAll.jpg',
       weighting: 100,
       number_of_likes: 2,
@@ -37,34 +37,40 @@ function App() {
       climbers: [],
     },
     {
-      id: 3,
+      number: 3,
       name: 'Tarzan',
       sector: 'Monkey Island',
       level: '3',
       handle_color: 'white',
       setter: 'Stefan Korder',
       tags: ['Travers'],
-      img_start: 'image.jpg',
+      img_start: 'Boulder-girl.jpg',
       img_all: 'imageAll.jpg',
       weighting: 100,
       number_of_likes: 2,
       start_date: '2022-04-26',
       expiry_date: '',
       climbers: [],
-    },
+    }
   ];
 
 
   return (
     <div className="App">
-    <ul>
-      {bouldersList.map(boulder => { return(
-        <li key={boulder.id}>
-          <p>{boulder.id}</p>
-          <p>{boulder.name}</p>
-        </li>
-      )})}
-    </ul>
+     <ul>
+      {bouldersList.map(boulder => (
+        <BoulderCard 
+        key={boulder.number}
+        id={boulder.number}
+        name={boulder.name}
+        sector={boulder.sector}
+        level={boulder.level}
+        handle_color={boulder.handle_color}
+        img_start={boulder.img_start}
+        likes={boulder.number_of_likes}
+        />
+      ))}
+     </ul>
     </div>
   );
 }
