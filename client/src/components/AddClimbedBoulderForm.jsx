5a4c3c86f81fd93f77df216a4cc3cb65d0953918
Antje Exner <antjeexner@Antjes-Air.fritz.box>
2022-04-29 22:32:40 +0200

    add saveClimbedBoulder function

--- client/src/components/AddClimbedBoulderForm.jsx
+++ client/src/components/AddClimbedBoulderForm.jsx
@@ -1,144 +1,180 @@
 import { useState } from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-const AddClimbedBoulderForm = () => {
- 
+const AddClimbedBoulderForm = ({saveClimbedBoulder}) => {
   const [date, setDate] = useState(new Date());
   const [projected, setProjected] = useState(false);
-  const [attempts, setAttempts] = useState();
+  const [attempts, setAttempts] = useState(0);
   const [result, setResult] = useState();
   const [liked, setLiked] = useState(false);
   const [levelFeedback, setLevelFeedback] = useState();
+  
 
   return (
-    <BoulderForm onSubmit={event => {}}>
-      <label htmlFor="date">Title:</label>
+    <BoulderForm
+      onSubmit={(event) => {event.preventDefault();
+        saveClimbedBoulder(date, projected, attempts, result, liked, levelFeedback);
+      }}
+    >
+      <label htmlFor="date">Climb date:</label>
       <input
         type="date"
         id="date"
         name="date"
-        placeholder=""
+        // defaultValue={date}
+        placeholder={date}
         value={date}
         onChange={event => {
           setDate(event.target.value);
         }}
       ></input>
-      <label htmlFor="projected">Projected:</label>
-      <input
-        type="checkbox"
-        id="projected"
-        name="projected"
-        placeholder=""
-        value={projected}
-        onChange={event => {
-          setProjected(event.target.value);
-        }}
-      ></input>
-      <label htmlFor="attempts">Attempts:</label>
-      <input
-        type="number"
-        id="attempts"
-        name="attempts"
-        placeholder="0"
-        value={attempts}
-        onChange={event => {
-          setAttempts(event.target.value);
-        }}
-      ></input>
       <div>
+        <label htmlFor="projected">Projected: </label>
         <input
-          type="radio"
-          id="zone"
-          name="result"
-          value="zone"
-          checked={result === 'zone'}
+          type="checkbox"
+          id="projected"
+          name="projected"
+          placeholder=""
+          value={projected}
           onChange={event => {
-            setResult(event.target.value);
+            setProjected(event.target.value);
           }}
-        />
-        <label for="zone">Zone</label>
+        ></input>
       </div>
+      <div>
+        <label htmlFor="attempts">Attempts: </label>
+        <input
+          type="number"
+          id="attempts"
+          name="attempts"
+          placeholder="0"
+          value={attempts}
+          onChange={event => {
+            setAttempts(event.target.value);
+          }}
+        ></input>
+      </div>
+      <fieldset>
+        <legend>Your result:</legend>
+        <div>
+          <input
+            type="radio"
+            id="zone"
+            name="result"
+            value="zone"
+            checked={result === 'zone'}
+            onChange={event => {
+              setResult(event.target.value);
+            }}
+          />
+          <label htmlFor="zone">Zone</label>
+        </div>
+
+        <div>
+          <input
+            type="radio"
+            id="top"
+            name="result"
+            value="top"
+            checked={result === 'top'}
+            onChange={event => {
+              setResult(event.target.value);
+            }}
+          />
+          <label htmlFor="top">Top</label>
+        </div>
+
+        <div>
+          <input
+            type="radio"
+            id="flash"
+            name="result"
+            value="flash"
+            checked={result === 'flash'}
+            onChange={event => {
+              setResult(event.target.value);
+            }}
+          />
+          <label htmlFor="flash">Flash</label>
+        </div>
+        <div>
+          <button
+            onClick={event => {
+              event.preventDefault();
+              setResult('');
+            }}
+          >
+            x
+          </button>
+        </div>
+      </fieldset>
 
       <div>
+        <label htmlFor="liked">Like: </label>
         <input
-          type="radio"
-          id="top"
-          name="result"
-          value="top"
-          checked={result === 'top'}
+          type="checkbox"
+          id="liked"
+          name="liked"
+          placeholder=""
+          value={liked}
           onChange={event => {
-            setResult(event.target.value);
+            setLiked(event.target.value);
           }}
-        />
-        <label for="top">Top</label>
+        ></input>
       </div>
 
-      <div>
-        <input
-          type="radio"
-          id="flash"
-          name="result"
-          value="flash"
-          checked={result === 'flash'}
-          onChange={event => {
-            setResult(event.target.value);
-          }}
-        />
-        <label for="flash">Flash</label>
-      </div>
+      <fieldset>
+        <legend>Level Feedback</legend>
+        <div>
+          <input
+            type="radio"
+            id="tooeasy"
+            name="levelFeedback"
+            value="tooeasy"
+            checked={levelFeedback === 'tooeasy'}
+            onChange={event => {
+              setLevelFeedback(event.target.value);
+            }}
+          />
+          <label htmlFor="tooeasy">Too easy</label>
+        </div>
 
-      <label htmlFor="liked">Like:</label>
-      <input
-        type="checkbox"
-        id="liked"
-        name="liked"
-        placeholder=""
-        value={liked}
-        onChange={event => {
-          setLiked(event.target.value);
-        }}
-      ></input>
-      <div>
-        <input
-          type="radio"
-          id="tooeasy"
-          name="levelFeedback"
-          value="tooeasy"
-          checked={levelFeedback === 'tooeasy'}
-          onChange={event => {
-            setLevelFeedback(event.target.value);
-          }}
-        />
-        <label for="tooeasy">Too easy</label>
-      </div>
+        <div>
+          <input
+            type="radio"
+            id="justright"
+            name="levelFeedback"
+            value="justright"
+            checked={levelFeedback === 'justright'}
+            onChange={event => {
+              setLevelFeedback(event.target.value);
+            }}
+          />
+          <label htmlFor="justright">Just right</label>
+        </div>
 
-      <div>
-        <input
-          type="radio"
-          id="justright"
-          name="levelFeedback"
-          value="justright"
-          checked={levelFeedback === 'justright'}
-          onChange={event => {
-            setLevelFeedback(event.target.value);
+        <div>
+          <input
+            type="radio"
+            id="toohard"
+            name="levelFeedback"
+            value="toohard"
+            checked={levelFeedback === 'toohard'}
+            onChange={event => {
+              setLevelFeedback(event.target.value);
+            }}
+          />
+          <label htmlFor="toohard">Too hard</label>
+        </div>
+        <button
+          onClick={event => {
+            event.preventDefault();
+            setLevelFeedback('');
           }}
-        />
-        <label for="justright">Just right</label>
-      </div>
-
-      <div>
-        <input
-          type="radio"
-          id="toohard"
-          name="levelFeedback"
-          value="toohard"
-          checked={levelFeedback === 'toohard'}
-          onChange={event => {
-            setLevelFeedback(event.target.value);
-          }}
-        />
-        <label for="toohard">Too hard</label>
-      </div>
+        >
+          x
+        </button>
+      </fieldset>
       <button>Save</button>
     </BoulderForm>
   );
@@ -146,11 +182,9 @@ const AddClimbedBoulderForm = () => {
 
 export default AddClimbedBoulderForm;
 
-
 const BoulderForm = styled.form`
+  margin: 0.5rem auto;
   max-width: 90%;
   display: flex;
   flex-direction: column;
 `;
-
-
--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,13 +1,32 @@
 import BoulderCard from '../components/BoulderCard';
 import AddClimbedBoulderForm from '../components/AddClimbedBoulderForm';
 import { useParams, Link } from 'react-router-dom';
+import { useState } from 'react';
+import styled from 'styled-components';
 
 const Add = ({bouldersList}) => {
     const { id } = useParams()
     const currentBoulder = bouldersList.find(boulder => boulder.number === Number(id));
-  return (
+
+    const [newClimbedBoulder, setNewClimbedBoulder] = useState();
+
+  const saveClimbedBoulder = (date, projected, attempts, result, liked, levelFeedback) => {
+    setNewClimbedBoulder({
+      id: '999999',
+      climber_id: '9999999999',
+      boulder_id: id,
+      date: date,
+      projected: projected,
+      attempts: attempts,
+      result: result,
+      liked: liked,
+      level_feedback: levelFeedback,
+    });
+  };
+  
+    return (
     <>
-      
+      <BoulderList role="list">
       {currentBoulder ? (
         <BoulderCard
           id={currentBoulder.number}
@@ -24,10 +43,22 @@ const Add = ({bouldersList}) => {
         />
         ) : (''
         )}
-        <AddClimbedBoulderForm/>
-      <Link to={`/`}>Back to Boulder List</Link>
+        </BoulderList>
+        <AddClimbedBoulderForm
+        saveClimbedBoulder={saveClimbedBoulder}/>
+        {newClimbedBoulder && (
+          <p>You have successfully saved a new entry for this boulder!</p>
+        )}
+        <Link to={`/`}>Back to Boulder List</Link>
     </>
   );
 };
+
+const BoulderList = styled.ul`
+  width: 95%;
+  display: flex;
+  flex-direction: column;
+  gap: 0.2rem;
+`
 
 export default Add;
