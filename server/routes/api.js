ba56bd0f9017cf79a9f0fed3196420cb60d09544
Antje Exner <antjeexner@Antjes-Air.fritz.box>
2022-05-03 15:50:07 +0200

    add post route and climbed_boulder schema

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,7 @@
 import express from 'express';
 const router = express.Router();
 import { Boulder } from '../models/boulder.js';
+import { Climbed_boulder } from '../models/climbed_boulder.js';
 
 router.get('/', (req, res, next) => {
   Boulder.find({})
@@ -17,6 +18,36 @@ router.get('/add/:id', (req, res, next) => {
   Boulder.findById(id)
     .then(data => {
       res.send(data);
+    })
+    .catch(() => {
+      next();
+    });
+});
+
+router.post('/add/', (req, res, next) => {
+  const climber_id = req.body.climber_id;
+  const boulder_id = req.body.boulder_id;
+  const date = req.body.date;
+  const projected = req.body.projected;
+  const attempts = req.body.attempts;
+  const result = req.body.result;
+  const liked = req.body.liked;
+  const level_feedback = req.body.level_feedback;
+
+  const newClimbed_boulder = Climbed_boulder({
+    climber_id,
+    boulder_id,
+    date,
+    projected,
+    attempts,
+    result,
+    liked,
+    level_feedback,
+  });
+  newClimbed_boulder
+    .save()
+    .then(data => {
+      res.status(201).send(data);
     })
     .catch(() => {
       next();
