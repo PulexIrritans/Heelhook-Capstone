636d760eafd6409f0e85806b6823d96353ef79b8
Antje Exner <antjeexner@Antjes-Air.fritz.box>
2022-05-05 13:49:07 +0200

    update Form story and test

--- client/src/components/AddClimbedBoulderForm.spec.jsx
+++ client/src/components/AddClimbedBoulderForm.spec.jsx
@@ -2,9 +2,19 @@ import AddClimbedBoulderForm from './AddClimbedBoulderForm';
 import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
 
+const climbedBoulder = {
+  projected: false,
+  attempts: 7,
+  result: 'top',
+  liked: true,
+  level_feedback: 0,
+};
+
 describe('AddClimbedBoulderForm', () => {
   it('renders 2 checkboxes, 6 radio buttons, 1 number input field, 2 reset buttons and a submit button', () => {
-    render(<AddClimbedBoulderForm />);
+    render(
+      <AddClimbedBoulderForm formPrefilledClimbedBoulder={climbedBoulder} />
+    );
 
     const checkboxesAll = screen.getAllByRole('checkbox');
     const radioButtonsAll = screen.getAllByRole('radio');
@@ -21,7 +31,12 @@ describe('AddClimbedBoulderForm', () => {
 
   it('submits form data if none of the input fields is filled out', () => {
     const handleAdd = jest.fn();
-    render(<AddClimbedBoulderForm saveClimbedBoulderToDatabase={handleAdd} />);
+    render(
+      <AddClimbedBoulderForm
+        formPrefilledClimbedBoulder={climbedBoulder}
+        saveClimbedBoulderToDatabase={handleAdd}
+      />
+    );
     const submitButton = screen.getByRole('button', { name: /Save/i });
 
     userEvent.click(submitButton);
