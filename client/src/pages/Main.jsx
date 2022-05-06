00719c45c90a172477c0ac2a2c720608fbfc6229
Antje Exner <antjeexner@Antjes-Air.fritz.box>
2022-05-06 10:42:46 +0200

    add main page and navigation to main

---
+++ client/src/pages/Main.jsx
@@ -0,0 +1,19 @@
+import Header from '../components/Header';
+import Navigation from '../components/Navigation';
+
+const Main = () => {
+  return (
+    <>
+      <Header title="Heelhook" />
+      <main>
+        <ul>
+          <li></li>
+          <li></li>
+        </ul>
+      </main>
+      <Navigation />
+    </>
+  );
+};
+
+export default Main;
