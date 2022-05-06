018ea55cd964efe5b5e11e08341532c46639746f
Antje Exner <antjeexner@Antjes-Air.fritz.box>
2022-05-06 13:16:10 +0200

    add doughnut chart climber overall

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,7 +1,12 @@
 import Header from '../components/Header';
 import Navigation from '../components/Navigation';
 import { useEffect, useState } from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+const URL = process.env.REACT_APP_URL;
 const USER_ID = 9999;
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Main = () => {
   const [climberOverallStats, setClimberOverallStats] = useState();
@@ -15,12 +20,41 @@ const Main = () => {
     fetchClimberOverallStats();
   }, []);
 
+  const data = {
+    labels: climberOverallStats?.map(stat => stat.label),
+    datasets: [
+      {
+        label: '# of Votes',
+        data: climberOverallStats?.map(stat => stat.amount),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.5)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <>
       <Header title="Heelhook" />
       <main>
         <ul>
-          <li></li>
+          <li>
+            <Doughnut data={data} />
+          </li>
           <li></li>
         </ul>
       </main>
