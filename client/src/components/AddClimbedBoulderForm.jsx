a0a2be0598f6357144321c9eb077923f6aa83475
Antje Exner <antjeexner@Antjes-Air.fritz.box>
2022-05-09 16:41:03 +0200

    fix attempts max input

--- client/src/components/AddClimbedBoulderForm.jsx
+++ client/src/components/AddClimbedBoulderForm.jsx
@@ -48,6 +48,7 @@ const AddClimbedBoulderForm = ({
           name="attempts"
           value={newAttempts}
           min="0"
+          max="99"
           onChange={event => {
             setNewAttempts(event.target.value);
           }}
