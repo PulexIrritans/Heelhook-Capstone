24220f02632f9a1c8208320d2c19f76bf285c70e
Antje Exner <antjeexner@Antjes-Air.fritz.box>
2022-05-21 10:29:20 +0200

    add not found page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Find from './pages/Find';
 import Add from './pages/Add';
 import Main from './pages/Main';
 import Profile from './pages/Profile';
+import NotFound from './pages/NotFoundPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/find" element={<Find />} />
         <Route path="/add/:id" element={<Add />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
---
+++ client/src/pages/NotFoundPage.jsx
@@ -0,0 +1,16 @@
+import Header from '../components/Header';
+import Navigation from '../components/Navigation';
+
+export default function NotFound() {
+  return (
+    <>
+      <Header title="Hellhook" />
+      <main>
+        <p>ERROR 404</p>
+        <p>🎉🥳</p>
+        <p>Page not found.</p>
+      </main>
+      <Navigation />
+    </>
+  );
+}
